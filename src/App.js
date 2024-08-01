d75b8b618307fe75adcef0003a9a554e0a4424ef
Alok3333 <alokprajapati697@gmail.com>
2024-08-01 10:36:30 +0530

    created one experimentlabs folder and done the setUp with react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ExperimentOne from "./ExperimentLabs/ExperimentOne";
+
+function App() {
+  return (
+    <div className="App">
+      <ExperimentOne />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ExperimentLabs/ExperimentOne.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExperimentOne() {
+  return <div>First Experiment</div>;
+}
+
+export default ExperimentOne;
